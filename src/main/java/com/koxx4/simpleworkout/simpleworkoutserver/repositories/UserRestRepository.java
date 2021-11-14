f52eef60d7d5515b720aad4bf81fed9b8803d339
koxx4 <piotr815@protonmail.com>
2021-11-14 18:29:09 +0100

    added some methods to rest repositories

--- src/main/java/com/koxx4/simpleworkout/simpleworkoutserver/repositories/UserRestRepository.java
+++ src/main/java/com/koxx4/simpleworkout/simpleworkoutserver/repositories/UserRestRepository.java
@@ -2,10 +2,13 @@ package com.koxx4.simpleworkout.simpleworkoutserver.repositories;
 
 import com.koxx4.simpleworkout.simpleworkoutserver.data.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(path = "users", collectionResourceRel = "users")
 @CrossOrigin
 public interface UserRestRepository extends PagingAndSortingRepository<User, Long> {
+    User findByNickname(@Param("nickname") String nickname);
+    User findByEmail(@Param("email") String email);
 }
